04eb85d1874882cc3b1c6dfc83dc18238d3b1bc8
em1455 <66853068+em1455@users.noreply.github.com>
2021-02-04 22:32:21 -0500

    new tabs + UI changes

--- pet-eth-app/packages/react-app/src/components/WalkTokenDetails.jsx
+++ pet-eth-app/packages/react-app/src/components/WalkTokenDetails.jsx
@@ -19,34 +19,35 @@ export const WalkTokenDetails = (props) => {
 
   return (
     <div>
+      <br></br>
         <Card>
-          <Card.Body>
+          <Card.Body className="customCard">
             <div class="container">
               <div class="row">
                 <div class="col-md">
                   <Row>
                     <Col>
-                      <Card.Title>Total Walk Tokens (WT)</Card.Title>
+                      <Card.Title className="customCardTitle">Total Walk Tokens (WT)</Card.Title>
                     </Col>
                   </Row>
-                  <Card.Text>
-                    <h6>+ 10 WT</h6>
+                  <Card.Text className="tokenFluctuation">+ 10 WT
                   </Card.Text>
-                  <Card.Text>
-                    <h1>3415</h1>
+                  <Card.Text className="walkTokenCount">
+                    3415
                   </Card.Text>
-                  <Card.Text>
-                    <h6>= XYZ USD</h6>
+                  <Card.Text className="usdConversion">
+                  = XYZ USD
                   </Card.Text>
                   <Row>
                     <Col>
-                      <Button>Shop Marketplace</Button>
-                      <Button
+                    <Container style={{display: "flex", justifyContent: "center", alignItems: "center" }} React Center>
+                    <Button style = {{fontSize: 14}}>Shop Marketplace</Button> &nbsp;&nbsp;&nbsp;
+                      <Button style = {{fontSize: 14}}
                         onClick={() => setModalShow(true)}
                         variant="secondary"
                       >
                         Redeem USD
-                      </Button>
+                      </Button >
                       <RedeemButton
                         show={modalShow}
                         onHide={() => setModalShow(false)}
@@ -54,10 +55,126 @@ export const WalkTokenDetails = (props) => {
                         walkExchange={props.walkExchange}
                         walkToken={props.walkToken}
                       />
+                    </Container>
                     </Col>
                   </Row>
                 </div>
 
+                <div class="col-md">
+                    <Tabs className="justify-content-center" defaultActiveKey="Home" 
+                          id="controlled-tab-example">
+                        <Tab eventKey="home" title="Home">
+                          <div style={{ marginTop: `12px` }}>
+                              <Table striped bordered hover>
+                                  <thead>
+                                    <tr>
+                                      <th>#</th>
+                                      <th>First Name</th>
+                                      <th>Last Name</th>
+                                      <th>Username</th>
+                                    </tr>
+                                  </thead>
+                                  <tbody >
+                                    <tr>
+                                      <td>1</td>
+                                      <td>Mark</td>
+                                      <td>Otto</td>
+                                      <td>@mdo</td>
+                                    </tr>
+                                    <tr>
+                                      <td>2</td>
+                                      <td>Jacob</td>
+                                      <td>Thornton</td>
+                                      <td>@fat</td>
+                                    </tr>
+                                    <tr>
+                                      <td>3</td>
+                                      <td colSpan="2">Larry the Bird</td>
+                                      <td>@twitter</td>
+                                    </tr>
+                                  </tbody>
+                              </Table>
+                          </div>
+                        </Tab>
+                        <Tab eventKey="profile" title="Profile">
+                        <div style={{ marginTop: `12px` }}>
+                              <Table striped bordered hover>
+                                  <thead>
+                                    <tr>
+                                      <th>#</th>
+                                      <th>zFirst Name</th>
+                                      <th>zLast Name</th>
+                                      <th>zUsername</th>
+                                    </tr>
+                                  </thead>
+                                  <tbody >
+                                    <tr>
+                                      <td>1</td>
+                                      <td>Mark</td>
+                                      <td>Otto</td>
+                                      <td>@mdo</td>
+                                    </tr>
+                                    <tr>
+                                      <td>2</td>
+                                      <td>Jacob</td>
+                                      <td>Thornton</td>
+                                      <td>@fat</td>
+                                    </tr>
+                                    <tr>
+                                      <td>3</td>
+                                      <td colSpan="2">Larry the Bird</td>
+                                      <td>@twitter</td>
+                                    </tr>
+                                  </tbody>
+                              </Table>
+                          </div>
+                        </Tab>
+                        <Tab eventKey="contact" title="Contact">
+                        <div style={{ marginTop: `12px` }}>
+                              <Table striped bordered hover>
+                                  <thead>
+                                    <tr>
+                                      <th>#</th>
+                                      <th>xFirst Name</th>
+                                      <th>xLast Name</th>
+                                      <th>xUsername</th>
+                                    </tr>
+                                  </thead>
+                                  <tbody >
+                                    <tr>
+                                      <td>1</td>
+                                      <td>Mark</td>
+                                      <td>Otto</td>
+                                      <td>@mdo</td>
+                                    </tr>
+                                    <tr>
+                                      <td>2</td>
+                                      <td>Jacob</td>
+                                      <td>Thornton</td>
+                                      <td>@fat</td>
+                                    </tr>
+                                    <tr>
+                                      <td>3</td>
+                                      <td colSpan="2">Larry the Bird</td>
+                                      <td>@twitter</td>
+                                    </tr>
+                                  </tbody>
+                              </Table>
+                          </div>
+                        </Tab>
+                    </Tabs>                    
+                </div>
+              </div>
+            </div>
+          </Card.Body>
+        </Card>
+
+        <br></br>
+
+        <Card>
+          <Card.Body>
+            <div class="container">
+              <div class="row">
                 <div class="col-md">
                     <Tabs className="justify-content-center" defaultActiveKey="Home" 
                           id="controlled-tab-example">
@@ -96,7 +213,9 @@ export const WalkTokenDetails = (props) => {
                         </Tab>
                         <Tab eventKey="profile" title="Profile">
                         </Tab>
-                        <Tab eventKey="contact" title="Contact" disabled>
+                        <Tab eventKey="contact" title="Contact">
+                        </Tab>
+                        <Tab eventKey="contact" title="Y33t">
                         </Tab>
                     </Tabs>                    
                 </div>
