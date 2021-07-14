6563376067f4f1c579f68b5d074a188f808abca2
Andrew <ath310@nyu.edu>
2021-07-14 13:30:34 -0400

    attempted changes

--- PetProject/hardhat.config.js
+++ PetProject/hardhat.config.js
@@ -28,7 +28,7 @@ module.exports = {
       },
     },
     mainnet: {
-      url: "https://mainnet.infura.io/v3/faefe1dcd6094fb388019173d2328d8f",
+      url: "https://mainnet.infura.io/v3/cc9ead11daad4fcf819ce82b503f1651", //new
       accounts: {
         mnemonic: mnemonic(),
       },
@@ -69,6 +69,9 @@ module.exports = {
       },
       {
         version:"0.7.0"
+      },
+      {
+        version:"0.8.4"
       }],
     optimizer: {
       enabled: true,
@@ -77,7 +80,8 @@ module.exports = {
   },
   etherscan: {
     apiKey: "FQKZCMUAQUA688R7FVDIH9BGD6698JIFPZ"
-    //npx hardhat verify --network rinkeby 0xD2820666665C127852213554E2B1cfA8A8199Ef8 "0xa55E01a40557fAB9d87F993d8f5344f1b2408072" "0x36bede640D19981A82090519bC1626249984c908" "0xF4C5310E51F6079F601a5fb7120bC72a70b96e2A" "0x745861AeD1EEe363b4AaA5F1994Be40b1e05Ff90" "EEEE ABNAEL MACHADO DE LIMA - CENE" "[5,7,10]" "[300,500,100]" "3" "500"
+    //npx hardhat verify --network mainnet 0x45Bd6a721582bc7C61562A6Cd4590c391899a759 "0xc3a9b2be34d7605eef2ec39c7f5b00e71552d319"
+    //npx hardhat verify --network mainnet 0xc3a9b2be34d7605eef2ec39c7f5b00e71552d319 "0xc3a9b2be34d7605eef2ec39c7f5b00e71552d319"
   },
   paths: {
     sources: "./contracts",
